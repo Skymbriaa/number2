bcb792c191f68790ff7a4ce60b52af73f11e8112
Skymbriaa <pozilaaskymbria@gmail.com>
2023-04-24 19:35:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.Write("Введіть число Х: ");
+
+        int x = int.Parse(Console.ReadLine());
+
+
+
+        if (x < 1)
+        {
+
+            Console.WriteLine("Помилка: введене число менше 1.");
+
+        }
+
+        else
+        {
+
+            int a = 0;
+
+            int b = 1;
+
+
+
+            Console.Write(a + " " + b + " ");
+
+
+
+            for (int i = 2; i < x; i++)
+            {
+
+                int c = a + b;
+
+                Console.Write(c + " ");
+
+                a = b;
+
+                b = c;
+
+            }
+
+        }
+
+    }
+
+}
